2d2d6e49c6cc3d495c89124be3f0b512f50368d0
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2012-12-05 00:01:40 +0530

    add make_torrc, documentation
    Given a config file, create a torrc. Add proper
    documentation.

--- tools.py
+++ tools.py
@@ -1,18 +1,52 @@
+from __future__ import with_statement
+
 import re
+import os
+import tempfile
 
 from PySide import QtCore
 
-def launch_tor(tor_cmd="tor", args=[]):
+def launch_tor(tor_cmd="tor", args=[], torrc_path=None):
     """
     Launches a tor process.
 
+    :param str tor_cmd: tor command to execute
+    :param list args: list of args to pass to the tor process
+    :param str torrc_path: path to torrc file
+
     :returns: **QtCore.QProcess** QProcess instance
     """
-    
+
+    config = {
+        'ControlPort': 9051,
+        'ControlListenAddress' : '127.0.0.1:9051'
+    }
+
+    if not torrc_path:
+        torrc_path = make_torrc(config)
+
+    args += ['-f', torrc_path]
     tor_process = QtCore.QProcess()
     tor_process.start(tor_cmd, args)
 
     return tor_process
+
+def make_torrc(config):
+    """
+    Creates a temporary torrc file.
+
+    :param dic config" config options for torrc
+
+    :returns: **str** path to the new torrc
+    """
+    torrc_path = tempfile.mkstemp(prefix = "torrc-", text = True)[1]
+
+
+    with open(torrc_path, "w") as torrc_file:
+        for key, value in config.items():
+            torrc_file.write("%s %s\n" % (key, value))
+
+    return torrc_path
 
 def parse_bootstrap_msg(msg):
     """
